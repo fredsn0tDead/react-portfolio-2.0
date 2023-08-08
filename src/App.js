1f902d4782fe32dced5020bdd0e7a6f4e986d091
Frederick Tetteh <frederick.tetteh@ontariotechu.net>
2023-08-08 15:02:45 -0400

    updated nav bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Home } from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+       <Navbar/>
+        <Home/>
+
+    </div>
+   
   );
 }
 
