6723777389c2f22c7da561c5106ead0d4bd9cacc
Frederick Tetteh <frederick.tetteh@ontariotechu.net>
2023-08-08 12:07:02 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h={80px} flex justify-between items-center px-4 bg={#0a192F}'>
+        
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
